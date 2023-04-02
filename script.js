ad5738c0ddf0cf292343fd5bbcad19477ad20ae4
almararn <almararn@gmail.com>
2023-04-02 13:10:46 +0000

    final

---
+++ script.js
@@ -0,0 +1,26 @@
+const harryPotter = {
+    firstName: 'Harry',
+    lastName: 'Potter',
+    hairColor: 'black'
+}
+
+const hermioneGranger = {
+    firstName: 'Hermione',
+    lastName: 'Granger',
+    hairColor: 'brown'
+}
+
+const ronWeasley = {
+    firstName: 'Ron',
+    lastName: 'Weasley',
+    hairColor: 'red'
+}
+
+const renderWizards = () => {
+    document.getElementById('harry').innerHTML =
+        `<span style="color:${harryPotter.hairColor}">${harryPotter.firstName} ${harryPotter.lastName}</span>`
+    document.getElementById('hermione').innerHTML =
+        `<span style="color:${hermioneGranger.hairColor}">${hermioneGranger.firstName} ${hermioneGranger.lastName}</span>`
+    document.getElementById('ron').innerHTML =
+        `<span style="color:${ronWeasley.hairColor}">${ronWeasley.firstName} ${ronWeasley.lastName}</span>`
+}
